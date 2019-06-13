c48154a830a67d03a8aa82fd9f19781d3932522d
abdoll78 <afalrohaily@gmail.com>
2019-06-13 12:28:21 +0300

    Update comic book detail view to be strongly typed

--- src/ComicBookGallary/Controllers/ComicBookController.cs
+++ src/ComicBookGallary/Controllers/ComicBookController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,18 +11,21 @@ namespace ComicBookGallary.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Dan Slott", Rule = "Script"},
+                    new Artist() {Name = "Humberto Ramos", Rule = "Pencils"},
+                    new Artist() {Name = "Victor Olazaba", Rule = "Inks"},
+                    new Artist() {Name = "Edgar Delgado", Rule = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos", Rule = "Letters"}
+                }
             };
-            return View();
+            return View(comicBook);
         }
     }
 }
